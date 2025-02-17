55c268e38f5c5cdc46c8a7b06ba2b86ba2a0fb72
Leoka2000 <reusoliveiraleo@gmail.com>
2025-02-17 02:43:38 +0100

    btn close

--- app/Http/Controllers/ShoppingCartController.php
+++ app/Http/Controllers/ShoppingCartController.php
@@ -59,4 +59,17 @@ class ShoppingCartController extends Controller
             'timeout' => 3000,
         ]);
     }
+//     koksd<form action="{{ route('cart.debug', ['id' => $item->id]) }}" method="GET">
+//     <button type="submit" class="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600">
+//       Debug Image
+//     </button>
+//   </form>
+
+//   <form action="{{ route('cart.remove', ['productId' => $item->product_id]) }}" method="POST">
+//     @csrf
+//     @method('DELETE')
+//     <x-mary-button type="submit" class="px-4 py-2 text-white bg-red-500 rounded-lg hover:bg-red-600">
+//       Remove Item
+//     </x-mary-button>
+//   </form> PARA DEBUGAR
 }
--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -91,12 +91,14 @@
       -->
                         <div class="flex w-full transform text-left text-base transition md:my-8 md:max-w-2xl md:px-4 lg:max-w-4xl">
                             <div class="relative flex w-full items-center overflow-hidden bg-white px-4 pt-14 pb-8 shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
+                                <a href="{{route('components.shopping_cart_component_index')}}">
                                 <button type="button" class="absolute top-4 right-4 text-gray-400 hover:text-gray-500 sm:top-8 sm:right-6 md:top-6 md:right-6 lg:top-8 lg:right-8">
                                     <span class="sr-only">Close</span>
                                     <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
                                     </svg>
                                 </button>
+                                </a>
 
                                 <div class="grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8">
 
